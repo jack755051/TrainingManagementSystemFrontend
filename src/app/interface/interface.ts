215f2fc22cce4402224281c3f41502bbe5c294ee
jack755051 <71447741+jack755051@users.noreply.github.com>
2023-09-12 01:53:36 +0800

    feat_20230912_員工專區_初建

---
+++ src/app/interface/interface.ts
@@ -0,0 +1,9 @@
+//員工專區 - 學習數據
+export interface learnData {
+  jobName: string;
+  InProgressAndDone: number;
+  OngoingCourses: number;
+  CompletedCourses: number;
+  TotalCourses: number;
+  PassRate: string;
+}
--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { learnData } from 'src/app/interface/interface';
+
+const ResponseData: learnData = {
+  jobName: 'JQCAJ-資訊工程師',
+  InProgressAndDone: 0,
+  OngoingCourses: 1,
+  CompletedCourses: 0,
+  TotalCourses: 4,
+  PassRate: '0%',
+};
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  learnData: learnData = ResponseData;
+  constructor() {}
 
+  ngOnInit(): void {
+    console.warn(this.learnData);
+  }
 }
